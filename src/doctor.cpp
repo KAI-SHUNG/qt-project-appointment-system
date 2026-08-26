4e73fcd6909a7f863f0237f64217d857959d19f0
KAI-SHUNG <3492562436@qq.com>
2026-08-26 21:18:52 +0800

    feat: add private function isConflict(), implement addTimeslot, test needed

---
+++ src/doctor.cpp
@@ -0,0 +1,53 @@
+#include "doctor.h"
+
+Doctor::Doctor(
+    const QString& name,
+    int age,
+    const Gender& gender,
+    const QString& doctorId,
+    const QString& title,
+    const QString& department
+    ):human(name, age,gender),
+    doctorId(doctorId),title(title),department(department)
+{}
+
+void Doctor::addTimeslot(const Timeslot& timeslot)
+{
+    if(this->isConflict(timeslot)){
+        throw std::invalid_argument("日程冲突！");
+    }
+
+    schedule.append(timeslot);
+    std::sort(schedule.begin(), schedule.end());
+}
+
+bool Doctor::isConflict(const Timeslot &timeslot) const
+{
+    for(auto& t: schedule){
+        if(t.overlaps(timeslot)){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+QString Doctor::getDoctorId() const
+{
+    return doctorId;
+}
+
+QString Doctor::getTitle() const
+{
+    return title;
+}
+
+QString Doctor::getDepartment() const
+{
+    return department;
+}
+
+const QList<Timeslot>& Doctor::getSchedule() const
+{
+    return schedule;
+}
---
+++ src/doctor.h
@@ -0,0 +1,41 @@
+#ifndef DOCTOR_H
+#define DOCTOR_H
+
+#include <QObject>
+#include <QDateTime>
+#include "human.h"
+#include "timeslot.h"
+
+class Doctor: public human
+{
+public:
+    Doctor(){}
+    Doctor(
+        const QString& name,
+        int age,
+        const Gender& gender,
+        const QString& doctorId,
+        const QString& title,
+        const QString& department
+        );
+
+    void addTimeslot(const Timeslot& timeslot);
+    void removeTimeslot(const Timeslot& timeslot);
+
+    QString getDoctorId() const;
+    QString getTitle() const;
+    QString getDepartment() const;
+
+    const QList<Timeslot>& getSchedule() const;
+
+private:
+    bool isConflict(const Timeslot& timeslot) const;
+
+    QString doctorId;
+    QString title;
+    QString department;
+
+    QList<Timeslot> schedule;
+};
+
+#endif // DOCTOR_H
