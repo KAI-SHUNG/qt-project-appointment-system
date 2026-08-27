5ff8defed30bc0c8507aec6c4e7b248ca430fc5d
KAI-SHUNG <3492562436@qq.com>
2026-08-27 10:37:09 +0800

    feat: add Doctor::removeTimeslot()

--- src/doctor.cpp
+++ src/doctor.cpp
@@ -21,9 +21,27 @@ void Doctor::addTimeslot(const Timeslot& timeslot)
     std::sort(schedule.begin(), schedule.end());
 }
 
+void Doctor::removeTimeslot(const Timeslot &timeslot)
+{
+    bool flag = false;
+    for(auto it = schedule.begin(); it != schedule.end(); it++){
+        if (timeslot == *it){
+            flag = true;
+            it = schedule.erase(it);
+        }
+    }
+    if (!flag){
+        qWarning() << "找不到时间段："
+                   << "dayOfWeek: " << timeslot.getDayOfWeek()
+                   << "startTime: " << timeslot.getStartTime()
+                   << "endTime: " << timeslot.getEndTime()
+                   << "capability: " << timeslot.getCapability();
+    }
+}
+
 bool Doctor::isConflict(const Timeslot &timeslot) const
 {
-    for(auto& t: schedule){
+    for(const auto& t: schedule){
         if(t.overlaps(timeslot)){
             return true;
         }
