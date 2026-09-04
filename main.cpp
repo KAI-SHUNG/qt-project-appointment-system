553ec8b80573ec607bfc9315ed21eff4513554ab
KAI-SHUNG <3492562436@qq.com>
2026-09-04 14:06:04 +0800

    fix(main.cpp): remove font setting in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 #include "ui/mainwindow.h"
 
 #include <QApplication>
-#include <QFont>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-
-    QFont font(QStringLiteral("Microsoft YaHei UI"), 10);
-    font.setStyleStrategy(QFont::PreferAntialias);
-    a.setFont(font);
 
     MainWindow w;
     w.show();
